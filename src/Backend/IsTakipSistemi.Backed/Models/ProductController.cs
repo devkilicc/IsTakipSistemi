f5e21757e41f458a82ddb4ab4647fe083262a6d5
Muhammed KILIC <devkilicc@gmail.com>
2025-01-02 22:50:45 +0300

    ProductController eklendi

---
+++ src/Backend/IsTakipSistemi.Backed/Models/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IsTakipSistemi.Backed.Models
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
